c8569e6cd242238d4373f828f8555ecee1fe951a
vsm <devops.july2.2017@gmail.com>
2023-02-01 08:39:06 +0530

    Update ecs-cfn.sh

--- ecs-cfn.sh
+++ ecs-cfn.sh
@@ -1,1 +1,2 @@
-
+#Create stack:  
+aws cloudformation create-stack --stack-name example-deployment --template-body file://./ecs.yml --capabilities CAPABILITY_NAMED_IAM --parameters 'ParameterKey=SubnetID,ParameterValue=subnet-e58fafa9'
